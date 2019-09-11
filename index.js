6fe4118eb989e70094003f0c931624c1dd1468ab
Muhammad Farhan Mahmood <farhanmahmood433@gmail.com>
2019-09-11 19:22:33 +0500

    Step_4 for the phonebook_backend project of Part_3 complete

--- index.js
+++ index.js
@@ -55,6 +55,15 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+
+    res.status(204).end()
+
+})
+
+
 
 
 const PORT = 3001
